81d34855e47c3364aaab3fc68fc957edc68e7678
APPRepo-maker <javitchandel@gmail.com>
2026-05-09 20:19:59 +0300

    session 2 task 1

---
+++ Weather advisor task.py
@@ -0,0 +1,12 @@
+temp = int(input("Enter temp: "))
+
+if temp >= 30:
+    print("hot day")
+elif temp >= 20:
+    print("warm day")
+elif temp >= 10:
+    print("cool day")
+else:
+    print("cold day")
+
+input("Press Enter")
